364ec3c3fdb2f5942ab5ca53adc331cebe14c837
wcrbrm <webcerebrium@gmail.com>
2017-05-09 09:29:34 +0300

    merged into single file

--- README.md
+++ README.md
@@ -5,11 +5,14 @@ A pair of scripts to take revision from the document and update it in local JSON
 useful in couch db batch files for initialization and database migrations.
 
 `node rev.js` - read revision from document JSON, provided in STDIN.
-`node rev-inject.js localfile.js` - read revision from STDIN and update it in the lcoal json document file.
+`node rev.js -inject localfile.js` - read revision from STDIN and update it in the lcoal json document file.
 
-Usage Example
+Batch File Example
 ===
 ```
-curl -s http://localhost:5984/mydb/_design/public | node rev.js | node rev-inject.js design_public.json
-curl -X PUT -H "Content-Type: application/json" http://localhost:5984/mydb/_design/public -d @design_public.json
+@set DB=mydb
+@set DOC=--Config
+@set FILENAME=--Config.json
+curl -s http://localhost:5984/%DB%/%DOC% | node rev.js | node rev -inject %FILENAME%
+curl -X PUT -H "Content-Type: application/json" http://localhost:5984/%DB%/%DOC% -d @%FILENAME%
 ```
--- rev.js
+++ rev.js
@@ -1,12 +1,60 @@
-let jsonData = '';
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) { jsonData += chunk; }
-});
+const fs = require('fs');
 
-process.stdin.on('end', () => {
-  const data = JSON.parse(jsonData);
-  process.stdout.write(data._rev);
-});
+const readRevisionFromStdin = (callback) => {
+  let rev = '';
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null) { rev += chunk; }
+  });
+  process.stdin.on('end', () => {
+    callback(rev.trim());
+  });
+}
+
+const readDocumentJson = (name, callback) => {
+  fs.readFile(name, 'utf8', (err, data) => {
+    if (err) throw err;
+    if (data._id) throw "Invalid Document Read, expected _id property";
+    if (typeof callback === 'function') callback(JSON.parse(data));
+  });  
+}
+
+const writeDocumentJson = (name, doc, callback) => {
+  fs.writeFile(name, JSON.stringify(doc, null, 2), {}, (err, data) => {
+    if (err) throw err;
+    if (typeof callback === 'function') callback(data);
+  });  
+};
+
+if (process.argv[2] === '-inject') {
+
+  const jsonFileName = process.argv[3];
+  if (!jsonFileName) { throw Exception('Json document is not provided'); }
+  readDocumentJson(jsonFileName, (doc) => {
+    // process.stdout.write(JSON.stringify(doc, null, 2) + "\n");   
+    readRevisionFromStdin((rev) =>  {
+      process.stdout.write('rev: ' + rev);
+      if (rev) {
+        doc._rev = rev;
+        writeDocumentJson(jsonFileName, doc);
+      }
+    });
+  });
+
+} else {
+
+  let jsonData = '';
+  process.stdin.setEncoding('utf8');
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if (chunk !== null) { jsonData += chunk; }
+  });
+  process.stdin.on('end', () => {
+    const data = JSON.parse(jsonData);
+    process.stdout.write(data._rev);
+  });
+
+}
+
 
